d4dcae0ee65e5e9b0b0a81a96be2058d4728f054
Philipp Stemberger <philipp.stemberger@gmail.com>
2020-11-27 17:57:29 +0100

    remote may files

--- src/components/h2.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function H2({title, ...props}) {
-    return <div className={'h2-container'}>
-        <h2>{title}</h2>
-        <div className='heading-line'> </div>
-    </div>;
-}
